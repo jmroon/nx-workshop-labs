473e42ee9596fed70a5a9a45d1b70affbc3f7566
jmroon <36086006+jmroon@users.noreply.github.com>
2021-03-15 13:06:58 -0400

    lab 5 checkpoint

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -3,15 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import {MatCardModule} from "@angular/material/card";
+import {MatCardModule} from '@angular/material/card';
+
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
 
 @NgModule({
   declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        RouterModule.forRoot([], {initialNavigation: 'enabled'}),
-        MatCardModule,
-    ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], {initialNavigation: 'enabled'}),
+    MatCardModule,
+    StoreUiSharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
